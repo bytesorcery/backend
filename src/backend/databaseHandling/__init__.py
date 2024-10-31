2e384561c6d160fbd049328777577bcc30c9c92a
jimothy <timothynewton1998@gmail.com>
2024-10-31 01:14:32 -0400

    Fixed parameter - added logic to execute schema if did not exist

--- src/backend/databaseHandling/__init__.py
+++ src/backend/databaseHandling/__init__.py
@@ -4,10 +4,15 @@ from sqlite3 import connect
 class DatabaseHandling:
     def __init__( self, database,
                   firstName, lastName,
-                  emailAddress, message, **kwargs
+                  eMail, message, **kwargs
     ):
+        
         db = connect(database)
         cursor = db.cursor()
+
+        #Create database schema if it doesn't exist
+        with open("src/backend/databaseHandling/schema.sql", "r") as f:
+            db.executescript(f.read())
 
         message = message.encode("utf=8")
 
@@ -28,7 +33,7 @@ class DatabaseHandling:
             """,
             ( firstName,
               lastName,
-              emailAddress,
+              eMail,
               phoneNumber,
               message      )
         )
